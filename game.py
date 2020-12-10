91dd7f8f23f86c13c3fd9a6dfda1988ed84925b0
Dhruv Sreenivas <dhruvsreenivas@Dhruvs-MBP.fios-router.home>
2020-12-10 07:12:05 -0500

    started work, wrote code for action set

---
+++ game.py
@@ -0,0 +1,100 @@
+import random
+
+
+class ChungToi:
+    def __init__(self):
+        # board description (c, o) in each cell indicates orientation and color of thing in cell (-1 if player 1, 1 if player 2, 0 if no one is there)
+        self.board = [([(0, 0)] * 3) for _ in range(3)]
+        self.colors = [-1, 1]
+        # -1 means diagonally, 1 means aligned with board axes
+        self.orientations = [0, 1]
+
+        # GAME STATE STUFF
+
+        # this keeps track of the number of pieces on the board
+        # only useful in the beginning when not all pieces are in play
+        self.color_pieces_on_board = [0, 0]
+
+        # keeps track of current player
+        self.curr_player = -1
+        self.player_positions = [[], []]
+
+    def action_set(self):
+        # if a player hasn't played all their pieces yet they must put a new piece in play
+        idx = 0 if self.curr_player == -1 else 1
+
+        if self.color_pieces_on_board[idx] < 3:
+            action_space = []
+            # has to put the piece somewhere on the board that isn't taken
+            # first part of the tuple is previous location, second part is destination
+            for i in range(3):
+                for j in range(3):
+                    if self.board[i][j] == (0, 0):
+                        action_space.append((None, ((i, j), -1)))
+                        action_space.append((None, ((i, j), 1)))
+
+            return action_space
+        else:
+            # all pieces are on board, there are only two courses of action
+            # either move horizontally, move diagonally, or jump
+
+            for r, c, o in self.player_positions[idx]:
+                if o == 1:
+                    # horizontal movement/jump
+                    action_space = []
+                    neighbors = [(r-1, c), (r, c-1), (r+1, c), (r, c+1)]
+                    for n in neighbors:
+                        if 0 <= n[0] and n[0] <= 2 and 0 <= n[1] and n[1] <= 2 and self.board[n[0]][n[1]] == (0, 0):
+                            # can move horizontally there, shouldn't run into out of bounds issues i think
+                            action_space.append(((r, c), (n, 1)))
+                            action_space.append(((r, c), (n, -1)))
+                        else:
+                            # have to jump horizontally
+                            o_n = ((r + 2 * (n[0] - r), c + 2 * (n[1] - c)))
+                            if 0 <= o_n[0] and o_n[0] <= 2 and 0 <= o_n[1] and o_n[1] <= 2 and self.board[o_n[0]][o_n[1]] == (0, 0):
+                                # this shouldn't run into any out of bounds issues
+                                action_space.append(((r, c), (o_n, 1)))
+                                action_space.append(((r, c), (o_n, -1)))
+
+                    return action_space
+                else:
+                    action_space = []
+                    # diagonal
+                    neighbors = [(r-1, c-1), (r+1, c+1),
+                                 (r-1, c+1), (r-1, c-1)]
+                    for n in neighbors:
+                        if 0 <= n[0] and n[0] <= 2 and 0 <= n[1] and n[1] <= 2 and self.board[n[0]][n[1]] == (0, 0):
+                            action_space.append(((r, c), (n, 1)))
+                            action_space.append(((r, c), (n, -1)))
+                        else:
+                            o_n = ((r + 2 * (n[0] - r), c + 2 * (n[1] - c)))
+                            if 0 <= o_n[0] and o_n[0] <= 2 and 0 <= o_n[1] and o_n[1] <= 2 and self.board[o_n[0]][o_n[1]] == (0, 0):
+                                action_space.append(((r, c), (o_n, 1)))
+                                action_space.append(((r, c), (o_n, -1)))
+
+                    return action_space
+
+    def act(self, action):
+        # action: (current_loc, (destination, orientation))
+        # assume here that action is in the game state's action_space (we assert it here)
+        assert action in self.action_set()
+
+        current_loc, dest_orient = action
+
+        idx = 0 if self.curr_player == -1 else 1
+
+        # clear out old player position
+        self.board[current_loc[0]][current_loc[1]] = (0, 0)
+        for tup in self.player_positions[idx]:
+            if tup[0] == current_loc[0] and tup[1] == current_loc[1]:
+                self.player_positions.remove(tup)
+                break
+
+        # put token at destination with new orientation
+        self.board[dest_orient[0][0]][dest_orient[0][1]] = (
+            self.curr_player, dest_orient[1])
+        self.player_positions.append(
+            (dest_orient[0][0], dest_orient[0][1], dest_orient[1]))
+
+        # change player
+        self.curr_player *= -1
