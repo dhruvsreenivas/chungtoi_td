1f1df22e72e2b7deea4804b80546f87333a8add9
Dhruv Sreenivas <dhruvsreenivas@Dhruvs-MBP.fios-router.home>
2020-12-11 19:35:08 -0500

    added value function update method

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from game import ChungToi
-from models import *
+from models import Agent
+
+game = ChungToi()
+agent = Agent(td_lam=0.7, lr=0.0012, eps=0.85)
+adversary_agent = Agent(td_lam=0.7, lr=0.0012, eps=0.15)
--- models.py
+++ models.py
@@ -6,8 +6,7 @@ import random
 
 
 class Agent:
-    def __init__(self, gamma, td_lam, lr, eps):
-        self.gamma = gamma
+    def __init__(self, td_lam, lr, eps):
         self.td_lam = td_lam
         self.lr = lr
         self.eps = eps  # this is for epsilon-greedy strategy with respect to value function given by network below
@@ -15,6 +14,7 @@ class Agent:
         # value network (make sure to initialize weights like in paper later on)
         self.linear1 = nn.Linear(20, 30, bias=True)
         self.linear2 = nn.Linear(30, 1)
+        self.loss = 0
 
         self.optimizer = torch.optim.Adam(lr=self.lr)
 
@@ -45,4 +45,17 @@ class Agent:
             # take random action
             return random.choice(action_set)
 
-    # now we need to write the function to update the value network
+    # do the action and legit change the state of the game
+    def act(self, game, action):
+        # take current value evaluation of state and add it to past value evaluations
+        value = self.forward(game.get_state())
+        self.loss *= self.td_lam
+        self.loss += value
+        game.act(action)
+
+    # update method uses the TD learning method that Tesauro used to learn TD-Gammon
+    def update_value_fn(self):
+        # every time you have a loss, you multiply the current loss value by lambda and add Y_t
+        self.optimizer.zero_grad()
+        self.loss.backward()
+        self.optimizer.step()
