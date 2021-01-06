02fa706d9c0a6296f1e43ff32599b3639b9ec731
Dhruv Sreenivas <dhruvsreenivas@Dhruvs-MacBook-Pro.local>
2021-01-06 17:55:31 -0500

    biffed

--- human_gameplay.py
+++ human_gameplay.py
@@ -7,6 +7,7 @@ def human_vs_machine(game, agent):
     # human plays against the MACHINE
     first = str(input('Do you want to go first? Y/N'))
     if first == 'Y':
-        action = tuple(input('Take an action in the game. An action is coded as (prev, next, orientation) where previous is the previous destination of token, next is the next destination, and orientation is the orientation wanted.'))
-        if action not in game.get_action_set(game.get_state()):
-            print('You cannot play this action--please choose another one.')
+        while not game.is_terminal()[0]:
+            action = tuple(input('Take an action in the game. An action is coded as (prev, next, orientation) where previous is the previous destination of token, next is the next destination, and orientation is the orientation wanted.'))
+            if action not in game.get_action_set(game.get_state()):
+                print('You cannot play this action--please choose another one.')
